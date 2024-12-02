c70029d3f5979308165a72642f786a69cab7b9f9
Áron Franta <aron.franta@ericsson.com>
2024-12-02 20:06:03 +0100

    day1 base

---
+++ day1/day1.py
@@ -0,0 +1,13 @@
+def __main__():
+    with open("input.txt") as f:
+        lines = f.readlines()
+
+    total = 0
+    for line in lines:
+        a, b = line.split()
+        
+
+    print(total)
+
+if __name__ == "__main__":
+    __main__()

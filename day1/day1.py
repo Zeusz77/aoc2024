2153f9093965640c944e3767881d5bb044ddc012
Zeusz77 <aron.franta@gmail.com>
2024-12-02 20:12:58 +0100

    day1 v0.1

--- day1/day1.py
+++ day1/day1.py
@@ -2,10 +2,23 @@ def __main__():
     with open("input.txt") as f:
         lines = f.readlines()
 
+    first_column = []
+    second_column = []
+
     total = 0
     for line in lines:
         a, b = line.split()
-        
+        first_column.append(int(a))
+        second_column.append(int(b))
+
+    first_column.sort()
+    second_column.sort()
+
+    for i in range(len(first_column)):
+        if first_column[i] >= second_column[i]:
+            total += first_column[i] - second_column[i]
+        else:
+            total += second_column[i] - first_column[i]
 
     print(total)
 
